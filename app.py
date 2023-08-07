d42e9530a2f8e030e8008583ddb767fb10982084
Adrià <adria.huertas@aetech.biz>
2023-08-07 12:03:31 +0200

    Carregada la dummy data a l'aplicació

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+# Load dummy data
+transactions_file = open("./dummy_data/transactions.json", "r")
+currency_rates_file = open("./dummy_data/currency_rates.json", "r")
+
+transactions = json.load(transactions_file)
+currency_rates = json.load(currency_rates_file)
+
+# Close files
+transactions_file.close()
+currency_rates_file.close()
 
 
 # Home route to check it's working
